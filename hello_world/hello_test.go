00e17b4ea3b3d7d092410d049830b249018640fa
Gerard Keown <gerard.keown@pytilia.io>
2022-09-16 22:57:48 +0100

    hello world after refactored tests

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,9 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+// testing.TB interface covers testing.T and testing.B
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// on failure, test reports failure from calling function line number, not here
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
