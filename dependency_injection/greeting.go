6fa94f392374389a5a78e9785a1ca8cd0eb57c7c
Gerard Keown <gerard.keown@pytilia.io>
2022-09-22 13:10:25 +0100

    chapter 8: dependency injection

---
+++ dependency_injection/greeting.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+// Greet sends a personalised greeting to writer.
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
