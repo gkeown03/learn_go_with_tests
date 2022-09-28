457408ec0a8c3c5f0c63fd1193b4139b567da41f
Gerard Keown <gerard.keown@pytilia.io>
2022-09-28 15:53:25 +0100

    chapter 15: templating with logic in templates

--- templates/blogrenderer.go
+++ templates/blogrenderer.go
@@ -1,8 +1,10 @@
 package blogrenderer
 
 import (
-	"fmt"
+	"embed"
+	"html/template"
 	"io"
+	"strings"
 )
 
 type Post struct {
@@ -12,26 +14,45 @@ type Post struct {
 	Tags        []string
 }
 
-func Render(w io.Writer, p Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", p.Title, p.Description)
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	if err := r.templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{sanitiseTitle .Title}}">{{.Title}}</a></li>{{end}}</ol>`
+
+	templ, err := template.New("index").Funcs(template.FuncMap{
+		"sanitiseTitle": func(title string) string {
+			return strings.ToLower(strings.Replace(title, " ", "-", -1))
+		},
+	}).Parse(indexTemplate)
 	if err != nil {
 		return err
 	}
 
-	_, err = fmt.Fprint(w, "Tags: <ul>")
-	if err != nil {
-		return err
-	}
-
-	for _, tag := range p.Tags {
-		_, err = fmt.Fprintf(w, "<li>%s</li>", tag)
-		if err != nil {
-			return err
-		}
-	}
-
-	_, err = fmt.Fprint(w, "</ul>")
-	if err != nil {
+	if err := templ.Execute(w, posts); err != nil {
 		return err
 	}
 
--- templates/blogrenderer_test.go
+++ templates/blogrenderer_test.go
@@ -2,8 +2,10 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
+	approvals "github.com/approvals/go-approval-tests"
 	"github.com/gkeown03/blogrenderer"
 )
 
@@ -17,19 +19,56 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
 
-		if err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+
+	t.Run("it renders an index of posts", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		posts := []blogrenderer.Post{{Title: "Hello World"}, {Title: "Hello World 2"}}
+
+		if err := postRenderer.RenderIndex(&buf, posts); err != nil {
 			t.Fatal(err)
 		}
 
 		got := buf.String()
-		want := `<h1>hello world</h1><p>this is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>`
+		want := `<ol><li><a href="/post/hello-world">Hello World</a></li><li><a href="/post/hello-world-2">Hello World 2</a></li></ol>`
 
 		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
+			t.Errorf("got %q want %q", got, want)
 		}
 	})
+}
+
+func BenchmarkRenderer(b *testing.B) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "this is a post",
+			Description: "this is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
