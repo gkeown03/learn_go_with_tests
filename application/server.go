345c9de0204ed7e3cfc8899665dd3bf23228f507
Gerard Keown <gerard.keown@pytilia.io>
2022-10-01 00:03:27 +0100

    application - introduce interface to handle player scores rather than hardcoding

--- application/server.go
+++ application/server.go
@@ -6,17 +6,15 @@ import (
 	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, GetPlayerScore(player))
+type PlayerStore interface {
+	GetPlayerScore(name string) int
 }
 
-func GetPlayerScore(player string) string {
-	if player == "Pepper" {
-		return "20"
-	}
-	if player == "Floyd" {
-		return "10"
-	}
-	return ""
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
